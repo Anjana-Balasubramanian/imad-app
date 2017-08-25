119dcdafd4be342173556a057f29d7d2c13369f4
Anjana-Balasubramanian <30565587+Anjana-Balasubramanian@users.noreply.github.com>
2017-08-25 17:06:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
+
+var config={
+    user:'anjanab2015it',
+    database:'anjanab2015it',
+    host:'db.imad.hasura-app.io',
+    port0:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -30,7 +39,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
